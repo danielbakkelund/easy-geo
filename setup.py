a62e150d265da3af8b26d4e59f544772e7d368fe
Daniel Bakkelund <daniel_bakkelund@hotmail.com>
2024-06-17 10:06:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
     name='easygeo',
     version='0.0.1',
-    packages=['easy_geo'],
+    packages=['easygeo'],
     install_requires=[
         'utm',
         'numpy'
